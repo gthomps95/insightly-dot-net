c7e4bdbf96cd2b147d54922e38d30db1d0eed85f
Greg Thompson <gthompson@horizontalsi.com>
2016-02-26 17:51:37 -0600

    Adding code to do some processing of rig data spreadsheet.

---
+++ RigDataUpdater/ExcelReader.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using Excel;
+using log4net;
+
+namespace RigDataUpdater
+{
+    public class ExcelReader
+    {
+        // ReSharper disable once UnusedMember.Local
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(ExcelReader));
+        private readonly string _filePath;
+
+        public ExcelReader(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public IEnumerable<RigData> RigData
+        {
+            get
+            {
+                using (var excelReader = GetExcelReader())
+                {
+                    //var count = 0;
+                    var dataSet = excelReader.AsDataSet();
+
+                    foreach (var table in dataSet.Tables.OfType<DataTable>())
+                    {
+                        foreach (var row in table.Rows.OfType<DataRow>().Skip(1))
+                        {
+                            var result = new RigData
+                            {
+                                Code = row[0].ToString().Trim(),
+                                ApiCode = row[1].ToString().Trim(),
+                                State = row[2].ToString().Trim(),
+                                District = row[3].ToString().Trim(),
+                                County = row[4].ToString().Trim(),
+                                Company = row[5].ToString().Trim(),
+                                Address = row[6].ToString().Trim(),
+                                City = row[7].ToString().Trim(),
+                                AddressState = row[8].ToString().Trim(),
+                                Zip = row[9].ToString().Trim(),
+                                Contact = row[10].ToString().Trim(),
+                                Phone = row[11].ToString().Trim(),
+                                Field = row[12].ToString().Trim(),
+                                Lease = row[13].ToString().Trim(),
+                                Legal = row[14].ToString().Trim(),
+                                Activity = row[15].ToString().Trim(),
+                                Comments = row[16].ToString().Trim(),
+                                Orientation = row[17].ToString().Trim()
+                            };
+
+                            yield return result;
+                        }
+                    }
+                }
+            }
+        }
+
+        private IExcelDataReader GetExcelReader()
+        {
+            return ExcelReaderFactory.CreateBinaryReader(File.Open(_filePath, FileMode.Open, FileAccess.Read));
+        }
+    }
+}
---
+++ RigDataUpdater/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using InsightlySDK;
+using log4net;
+using LinqKit;
+
+namespace RigDataUpdater
+{
+    class Program
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(Program));
+        private static Insightly Insightly { get; set; }
+
+        public static void Main(string[] args)
+        {
+            ConfigureLog4Net();
+            Logger.InfoFormat("Starting version {0}...", GetVersion());
+
+            var apiKey = args[0];
+            Insightly = new Insightly(apiKey);
+
+            var program = new Program();
+            program.Execute();
+
+            Console.WriteLine("Press enter to continue...");
+            Console.Read();
+        }
+
+        private static void ConfigureLog4Net()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+        }
+
+        private void Execute()
+        {
+            var orgs = GetOrganizations().ToList();
+            Logger.InfoFormat("Found {0} orgs", orgs.Count());
+
+            ProcessRigData(orgs);
+        }
+
+        private void ProcessRigData(List<InsightlyOrg> orgs)
+        {
+            var reader = new ExcelReader(@"d:\rig_data\1ILP_20160226.XLS");
+            var notFound = 0;
+            var found = 0;
+            var foundOrgs = new List<string>();
+
+            File.Delete(@"d:\rig_data\orgs_nf.txt");
+            File.Delete(@"d:\rig_data\orgs_f.txt");
+
+            foreach (var rigData in reader.RigData.Where(rd => rd.Orientation.Equals("HOR")))
+            {
+                var searchName = MakeSearchName(rigData.Company);
+                if (foundOrgs.Contains(searchName))
+                    continue;
+                foundOrgs.Add(searchName);
+
+                var org = orgs.FirstOrDefault(o => o.OrgSearchName == searchName);
+
+                if (org == null)
+                {
+                    notFound++;
+                    Logger.InfoFormat("Org {0} was not found.", rigData.Company);
+                    File.AppendAllText(@"d:\rig_data\orgs_nf.txt", $"{rigData.Company}|{rigData.State}|{rigData.County}|{rigData.Contact}\n");
+                }
+                else
+                {
+                    found++;
+                    Logger.InfoFormat("Org {0} was found, id is {1}", rigData.Company, org.OrgId);
+                    File.AppendAllText(@"d:\rig_data\orgs_f.txt", $"{rigData.Company}|{org.OrgId.ToString()}|{org.OrgName}\n");
+                }
+
+            }
+
+            Logger.InfoFormat("Completed file.  Found: {0}, Not found: {1}.", found, notFound);
+
+        }
+
+        private IEnumerable<InsightlyOrg> GetOrganizations()
+        {
+            var orgs = Insightly.GetOrganizations();
+
+            return orgs.Select(o => new InsightlyOrg
+            {
+                OrgId = Convert.ToInt32(o["ORGANISATION_ID"]),
+                OrgName = o["ORGANISATION_NAME"].ToString(),
+                OrgSearchName = MakeSearchName(o["ORGANISATION_NAME"].ToString())
+            });
+        }
+
+        private string MakeSearchName(string name)
+        {
+            var result = name
+                .ToLower();
+
+            var excessWords = new[] {"oil", "gas", "energy", "petroleum",
+                "production", "operating", "exploration", "development", "investments", "resources",
+                "group", "company", "inc", "llc", "usa", "co.", "lp"};
+
+            excessWords.ForEach(word => result = result.Replace(word, ""));
+
+            const string specialChars = " -,.()&";
+            specialChars.ForEach(c => result = result.Replace(c.ToString(), ""));
+
+            return result;
+        }
+        public static String GetVersion()
+        {
+            return FileVersionInfo.GetVersionInfo(System.Reflection.Assembly.GetExecutingAssembly().Location).FileVersion;
+        }
+    }
+
+    public class InsightlyOrg
+    {
+        public int OrgId { get; set; }
+        public string OrgName { get; set; }
+        public string OrgSearchName { get; set; }
+    }
+
+    public class RigData
+    {
+        //CODE1	API	STATE	DISTRICT	COUNTY	COM	AD1	CIT	STA	ZIP	CONTACTS	TL1	Field	LEASE	LEGAL	ACTIVITY	COMMENTS	wellorient
+        public string RowNum { get; set; }
+        public string Code { get; set; }
+        public string ApiCode { get; set; }
+        public string State { get; set; }
+        public string District { get; set; }
+        public string County { get; set; }
+        public string Company { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string AddressState { get; set; }
+        public string Zip { get; set; }
+        public string Contact { get; set; }
+        public string Phone { get; set; }
+        public string Field { get; set; }
+        public string Lease { get; set; }
+        public string Legal { get; set; }
+        public string Activity { get; set; }
+        public string Comments { get; set; }
+        public string Orientation { get; set; }
+    }
+}
